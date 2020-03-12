dc93bed1c571922d939668fef5f82993777dfe41
Semert <semert2121@gmail.com>
2020-03-12 20:11:42 +0300

    'StackQuestionAdded'

--- Data Structures/StackQuestion.py
+++ Data Structures/StackQuestion.py
@@ -51,3 +51,23 @@ print(is_paren_balanced("(((({}))))"))
 
 print(is_paren_balanced("[][]]]"))
 print(is_paren_balanced("[][]"))
+
+#---------------------------------------
+
+#
+# def div_by_2(dec_num):
+#     s = Stack()
+#
+#     while dec_num > 0:
+#         remainder = dec_num % 2
+#         s.push(remainder)
+#         dec_num = dec_num // 2
+#
+#     bin_num = ""
+#     while not s.is_empty():
+#         bin_num += str(s.pop())
+#     return bin_num
+#
+# print(div_by_2(242))
+
+---------------------------------------
