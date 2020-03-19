d8bd4df70685cdf40efac1ac44e74b339f5c0cfa
Semert <semert2121@gmail.com>
2020-03-19 15:12:27 +0300

    List Compherension

--- Algorithms/List compherension.py
+++ Algorithms/List compherension.py
@@ -17,3 +17,16 @@ print([n for n in nums])
 # Using a map + lambda
 # my_list = map(lambda n: n*n, nums)
 # print(my_list)
+
+
+
+# I want 'n' for each 'n' in nums if 'n' is even
+# my_list = []
+# for n in nums:
+#   if n%2 == 0:
+#     my_list.append(n)
+# print(my_list)
+
+# Using a filter + lambda
+# my_list = filter(lambda n: n%2 == 0, nums)
+# print(my_list)
