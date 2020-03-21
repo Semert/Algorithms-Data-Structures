2a171f0de5caa4acd9117c4e7cdc4ebdebaa3cd1
Semert <semert2121@gmail.com>
2020-03-21 22:31:14 +0300

    some changes

--- Data Structures/Linked List.py
+++ Data Structures/Linked List.py
@@ -78,4 +78,56 @@ llist.display()
 #             total += 1
 #             curNode = curNode.next
 #         return total
+#
+#     def display(self):
+#         elements = []
+#         curNode = self.head
+#         while curNode.next!=None:
+#             curNode = curNode.next
+#             elements.append(curNode.data)
+#         print(elements)
+#
+#     def get(self,index):
+#         if index >= self.length():
+#             print("Error Out of Index")
+#             return None
+#         curIndex = 0
+#         curNode = self.head
+#         while True:
+#             curNode = curNode.next
+#             if(curIndex == index):
+#                 return curNode.data
+#             curIndex += 1
+#
+#
+#     def erase(self,index):
+#         if index >= self.length():
+#             print("Error Out of Index")
+#             return None
+#         curIndex = 0
+#         curNode = self.head
+#         while True:
+#             lastNode = curNode
+#             curNode = curNode.next
+#             if curIndex==index:
+#                 lastNode.next = curNode.next
+#                 return
+#             curIndex += 1
+#
+#
+# myList = LinkedList()
+#
+# myList.display()
+#
+# myList.append(1)
+# myList.append(2)
+# myList.append(3)
+#
+# myList.display()
+# print("element at 2nd index %d" % myList.get(2))
+#
+# myList.erase(1)
+# myList.display()
 
+
+# New Linked list script will be added.
