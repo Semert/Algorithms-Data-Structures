172583636fb34fcb4743cbd47edfab6357e679c4
Semert <semert2121@gmail.com>
2020-03-22 23:15:15 +0300

    New Files

--- Algorithms/Test.py
+++ Algorithms/Test.py
@@ -10,23 +10,3 @@ ar = [3, 2, 1, 3]
 # print()
 # print(d*5-a)
 
-lst = [73, 67, 38, 33]
-lst2 = [37, 38]
-def gradingStudents(grades):
-    grades1 = []
-    gradeler = []
-    for a in grades:
-        b = a%5
-        c = a-b
-        beslikler = c + 5
-
-        if(beslikler-a<3 and beslikler>38):
-            grades1.append(int(beslikler))
-        else:
-           grades1.append(a)
-
-
-    print(grades1)
-print(gradingStudents(lst))
-
- 
