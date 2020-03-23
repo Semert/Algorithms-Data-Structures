9c180e2cf6b8a82cfeb67dd3337bc5ef46c560bd
Semert <semert2121@gmail.com>
2020-03-23 21:52:51 +0300

    Array Arbitrary

--- Arrays/ArbitraryPrecisionArithmetic.py
+++ Arrays/ArbitraryPrecisionArithmetic.py
@@ -3,3 +3,15 @@ A2 = [9, 9, 9]
 
 # s = ''.join(map(str, A))
 # print(int(s) + 1)
+
+def plus_one(A):
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i-1] += 1
+    if A[0] == 10:
+        A[0] = 1
+        A.append(0)
+    return A
