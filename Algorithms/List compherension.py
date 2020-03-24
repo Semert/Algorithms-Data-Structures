17645a7190de37b73d7c495f00ee1561e5a3b00f
Semert <semert2121@gmail.com>
2020-03-24 14:41:20 +0300

    Study

--- Algorithms/List compherension.py
+++ Algorithms/List compherension.py
@@ -8,15 +8,15 @@ print(my_list)
 
 print([n for n in nums])
 
-# I want 'n*n' for each 'n' in nums
-# my_list = []
-# for n in nums:
-#   my_list.append(n*n)
-# print(my_list)
+I want 'n*n' for each 'n' in nums
+my_list = []
+for n in nums:
+  my_list.append(n*n)
+print(my_list)
 
-# Using a map + lambda
-# my_list = map(lambda n: n*n, nums)
-# print(my_list)
+Using a map + lambda
+my_list = map(lambda n: n*n, nums)
+print(my_list)
 
 
 
@@ -39,8 +39,8 @@ print([n for n in nums])
 # print(my_list)
 
 # Dictionary Comprehensions
-names = ['Bruce', 'Clark', 'Peter', 'Logan', 'Wade']
-heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
+# names = ['Bruce', 'Clark', 'Peter', 'Logan', 'Wade']
+# heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
 # print zip(names, heros)
 
 # I want a dict{'name': 'hero'} for each name,hero in zip(names, heros)
@@ -61,7 +61,7 @@ heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
 
 # Generator Expressions
 # I want to yield 'n*n' for each 'n' in nums
-nums = [1,2,3,4,5,6,7,8,9,10]
+# nums = [1,2,3,4,5,6,7,8,9,10]
 
 # def gen_func(nums):
 #     for n in nums:
