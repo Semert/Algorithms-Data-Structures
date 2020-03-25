a94e960c917ae607b17079a3ac2f35edacfcf1c4
Semert <semert2121@gmail.com>
2020-03-25 22:45:49 +0300

    List

--- Algorithms/List compherension.py
+++ Algorithms/List compherension.py
@@ -51,13 +51,3 @@ print([n for n in nums])
 
 # If name not equal to Peter
 
-# Set Comprehensions
-# nums = [1,1,2,1,3,4,3,4,5,5,6,7,8,7,9,9]
-# my_set = set()
-# for n in nums:
-#     my_set.add(n)
-# print(my_set)
-#
-# a = set(nums)
-# print(a)
-
