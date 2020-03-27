a4a56ee1ab076b06837f47b1c0c176a75fcef22d
Semert <semert2121@gmail.com>
2020-03-27 14:43:42 +0300

    Selection SORT Duplicate Values

--- Sort/SelectionSort.py
+++ Sort/SelectionSort.py
@@ -1,17 +1,23 @@
 list1 = [11,3,2,55,7,0]
-print(list1)
+print("Unsorted list ", list1)
 
 for i in range(len(list1)):
     min_value = min(list1[i:]) # Her aşamada i kadar element atlayacak.
     min_index = list1.index(min_value)
     list1[i],list1[min_index] = list1[min_index],list1[i]
 
-print(list1)
+print("Sorted list ", list1)
 
+print("--------------------------------------")
 # Works For Duplicate values
+list1 = [11,0,3,2,2,55,7,0]
+print("Unsorted list ", list1)
 
 for i in range(len(list1)):
     min_value = min(list1[i:]) # Her aşamada i kadar element atlayacak.
     min_index = list1.index(min_value,i)
     list1[i],list1[min_index] = list1[min_index],list1[i]
+print("Sorted List", list1)
+print("--------------------------------------")
+
 
