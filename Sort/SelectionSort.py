9ff5de2b112644dad89fe3f55e5b6016a395c4fd
Semert <semert2121@gmail.com>
2020-03-27 14:50:34 +0300

    Selection SORT swap numbers

--- Sort/SelectionSort.py
+++ Sort/SelectionSort.py
@@ -13,11 +13,25 @@ print("--------------------------------------")
 list1 = [11,0,3,2,2,55,7,0]
 print("Unsorted list ", list1)
 
-for i in range(len(list1)):
+for i in range(len(list1)):  # -1 yaparsak son elemen zaten sort olmuş olacağı için bir sorun olmaz.
+
     min_value = min(list1[i:]) # Her aşamada i kadar element atlayacak.
     min_index = list1.index(min_value,i)
     list1[i],list1[min_index] = list1[min_index],list1[i]
 print("Sorted List", list1)
+
+for i in range(len(list1)-1):  # -1 yaparsak son elemen zaten sort olmuş olacağı için bir sorun olmaz.
+
+    min_value = min(list1[i:]) # Her aşamada i kadar element atlayacak.
+    min_index = list1.index(min_value,i)
+    if list1[i] != list1[min_index]: #Aynı sayıları swap etmesine gerek yok.
+        list1[i],list1[min_index] = list1[min_index],list1[i]
+print("Sorted List", list1)
+
+
+
 print("--------------------------------------")
+
+
 
 
