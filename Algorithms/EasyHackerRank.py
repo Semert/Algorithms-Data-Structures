73c3b31b5e3080c0e4d8756ff6e4d6067777a338
Semert <semert2121@gmail.com>
2020-03-29 23:26:39 +0300

    Some Fix

--- Algorithms/EasyHackerRank.py
+++ Algorithms/EasyHackerRank.py
@@ -5,31 +5,31 @@
 #arr = [1,2,3,4,5]
 arr = [7,69,2,221,8974]
 
-def miniMaxSum(arr):
-    arr.sort()
-    array = arr.copy()
-    a = []
-    b = []
-
-    def funcMax():
-        max = 0
-        for i in range(len(arr)):
-            if ( arr[i] > max):
-                max = arr[i]
-        return max
-
-    def funcMin():
-        min = arr[-1]
-        for i in range(len(array)):
-            if ( array[i] < min):
-                min = array[i]
-        return min
-    for i in range(1,len(array)):
-        array.remove(funcMin())
-        b.append(funcMin())
-
-    for i in range(1,len(arr)):
-        arr.remove(funcMax())
-        a.append(funcMax())
-    print(sum(a),sum(b))
+# def miniMaxSum(arr):
+#     arr.sort()
+#     array = arr.copy()
+#     a = []
+#     b = []
+#
+#     def funcMax():
+#         max = 0
+#         for i in range(len(arr)):
+#             if ( arr[i] > max):
+#                 max = arr[i]
+#         return max
+#
+#     def funcMin():
+#         min = arr[-1]
+#         for i in range(len(array)):
+#             if ( array[i] < min):
+#                 min = array[i]
+#         return min
+#     for i in range(1,len(array)):
+#         array.remove(funcMin())
+#         b.append(funcMin())
+#
+#     for i in range(1,len(arr)):
+#         arr.remove(funcMax())
+#         a.append(funcMax())
+#     print(sum(a),sum(b))
 #-----------------------------------------
