70d361f6b0213360a551dfa50239fe238d869452
Semert <semert2121@gmail.com>
2020-03-29 23:26:08 +0300

    Some Fix

--- Sort/SelectionSort.py
+++ Sort/SelectionSort.py
@@ -1,12 +1,12 @@
 list1 = [11,3,2,55,7,0]
 print("Unsorted list ", list1)
 
-# for i in range(len(list1)):
-#     min_value = min(list1[i:]) # Her aşamada i kadar element atlayacak.
-#     min_index = list1.index(min_value)
-#     list1[i], list1[min_index] = list1[min_index], list1[i]
-#
-# print("Sorted list ", list1)
+for i in range(len(list1)):
+    min_value = min(list1[i:]) # Her aşamada i kadar element atlayacak.
+    min_index = list1.index(min_value)
+    list1[i], list1[min_index] = list1[min_index], list1[i]
+
+print("Sorted list ", list1)
 
 print("--------------------------------------")
 
