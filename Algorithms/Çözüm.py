ffd64ad5a0c599a225264c2d0eee407204a000bc
Semert <semert2121@gmail.com>
2020-04-07 17:17:25 +0300

    LeetCode MaxSubArray

--- Algorithms/Çözüm.py
+++ Algorithms/Çözüm.py
@@ -18,7 +18,9 @@
 #
 # print(isHappy(n)),
 
-nums = [1,2,0,3,0,4]
+#--------------------------------
+# Move Zeros
+# nums = [1,2,0,3,0,4]
 # a.remove(0)
 # print(a)
 
@@ -29,13 +31,27 @@ nums = [1,2,0,3,0,4]
 #         nums.append(0)
 # print(nums)
 
-j = 0  # position of 1st 0
+# j = 0  # position of 1st 0
+#
+# for i in range(len(nums)):
+#     if nums[i] != 0:
+#         print(nums,i,j)
+#         print(nums[i],nums[j])
+#         nums[i], nums[j] = nums[j], nums[i]
+#         j += 1
+#
+# print(nums)
 
-for i in range(len(nums)):
-    if nums[i] != 0:
-        print(nums,i,j)
-        print(nums[i],nums[j])
-        nums[i], nums[j] = nums[j], nums[i]
-        j += 1
+#--------------------------------
+# Maximum SubArray
+def maxSubArray(nums):
+    # we are taking two varible total_sum and max_sum
+    # total_sum will take care of sum till every index and max_sum will the max(total_sum, max_sum)
+    total_sum = max_sum = nums[0]
+    # in starting we are giving value to both total_sum and max_sum as value at index 0
+    for i in nums[1:]:  # looping the array starting from index 1
 
-print(nums)
+        total_sum = max(i, total_sum + i)  # computing total sum
+        max_sum = max(total_sum, max_sum)  # updating max_sum if total_sum is larger than max_sum
+
+    return max_sum
