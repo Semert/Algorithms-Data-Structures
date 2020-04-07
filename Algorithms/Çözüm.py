15a3f0839b3b3fce364c76273b3c44a11b865499
Semert <semert2121@gmail.com>
2020-04-07 16:23:52 +0300

    LeetCode MoveZeros

--- Algorithms/Çözüm.py
+++ Algorithms/Çözüm.py
@@ -16,4 +16,26 @@
 #             return False
 #
 #
-# print(isHappy(n))
+# print(isHappy(n)),
+
+nums = [1,2,0,3,0,4]
+# a.remove(0)
+# print(a)
+
+#
+# for i in nums:
+#     if i==0:
+#         nums.remove(i)
+#         nums.append(0)
+# print(nums)
+
+j = 0  # position of 1st 0
+
+for i in range(len(nums)):
+    if nums[i] != 0:
+        print(nums,i,j)
+        print(nums[i],nums[j])
+        nums[i], nums[j] = nums[j], nums[i]
+        j += 1
+
+print(nums)
