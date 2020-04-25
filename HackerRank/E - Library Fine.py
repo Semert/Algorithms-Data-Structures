4129414b0fa946a3961e4da13b167eba875b6eb1
Semert <semert2121@gmail.com>
2020-04-25 22:00:20 +0300

    Library Fine

--- HackerRank/E - Library Fine.py
+++ HackerRank/E - Library Fine.py
@@ -1,3 +1,4 @@
+# https://www.hackerrank.com/challenges/library-fine/problem
 def libraryFine(d1, m1, y1, d2, m2, y2):
 
     if(m1==m2 and y1==y2 and d1>d2):
@@ -7,3 +8,18 @@ def libraryFine(d1, m1, y1, d2, m2, y2):
     elif(y1>y2):
         return 10000
     return 0
+
+    if (y2 == y1):
+        if (m2 == m1):
+            if (d2 >= d1):
+                return 0
+            else:
+                return (d1 - d2) * 15
+        elif (m2 > m1):
+            return 0
+        else:
+            return (m1 - m2) * 500
+    elif (y2 > y1):
+        return 0
+    else:
+        return 10000
