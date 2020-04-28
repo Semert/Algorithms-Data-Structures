a828611851e16445b7d8dc134f84e9e90ca7f40b
Semert <semert2121@gmail.com>
2020-04-28 23:29:19 +0300

    new questions

---
+++ HackerRank/E - Equalize the Array.py
@@ -0,0 +1,30 @@
+# https: // www.hackerrank.com / challenges / equality - in -a - array / problem
+
+
+def equalizeArray(arr):
+    ht = dict()
+    count = 0
+    for a in arr:
+        if a in ht:
+            ht[a] += 1
+        else:
+            ht[a] = 1
+    maxocc = max(ht.values())
+    for k, v in ht.items():
+        if (v < maxocc):
+            count += arr.count(k)
+    return count
+
+    max_c = -1
+    max_item = None
+    count = {}
+    for item in aRR:
+        if item in count:
+            count[item] += 1
+        else:
+            count[item] = 1
+        if count[item] > max_c:
+            max_c = count[item]
+            max_item = item
+
+    return (len(aRR) - max_c)
