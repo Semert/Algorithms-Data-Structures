4e4db8587f6aca7ed250f526f519311dd8b95694
Semert <semert2121@gmail.com>
2020-05-01 21:08:32 +0300

    Solved Questions

---
+++ HackerRank/E - Mini-Max Sum.py
@@ -0,0 +1,16 @@
+https://www.hackerrank.com/challenges/mini-max-sum/problem
+def miniMaxSum(arr):
+    arr = sorted(arr)
+    print(sum(arr[:-1]),sum(arr[1:])
+
+#Solution 2
+s = sum(arr)
+minmax = []
+for i in arr:
+    minmax.append(s-i)
+print(min(minmax), max(minmax))
+# Solution 3
+l=[]
+for i in range(len(arr)):
+		l.append(sum(arr)-arr[i])
+print(min(l),max(l))
