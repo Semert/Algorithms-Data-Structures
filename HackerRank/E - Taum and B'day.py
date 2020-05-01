ff75a9c4a05ae7bed4cf6640456266cb366ed28d
Semert <semert2121@gmail.com>
2020-05-01 21:07:35 +0300

    Solved Questions

---
+++ HackerRank/E - Taum and B'day.py
@@ -0,0 +1,10 @@
+https://www.hackerrank.com/challenges/taum-and-bday/problem
+def taumBday(b, w, bc, wc, z):
+    diff=abs(bc-wc)
+    if diff<=z:
+        return b*bc+w*wc
+    else:
+        if bc>wc:
+            return (b+w)*wc+b*z
+        else:
+            return (b+w)*bc+w*z
