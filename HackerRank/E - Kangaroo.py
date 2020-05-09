afda6d3bdbb2368a653d81990080c01e228744b6
Semert <semert2121@gmail.com>
2020-05-09 21:09:54 +0300

    Kangaroo Question Solution 2

--- HackerRank/E - Kangaroo.py
+++ HackerRank/E - Kangaroo.py
@@ -9,4 +9,23 @@ def kangaroo(x1, v1, x2, v2):
     if d > 0:
         if s1%d == 0:
             return 'YES'
-    return 'NO
+    return 'NO'
+
+# Solution 2
+i,count=1,False
+    if v1<=v2 and x1<x2:
+        return "NO"
+    elif v1>v2 and x1>x2:
+        return "NO"
+    elif v1>v2 and x1<x2:
+        while((x1+(i*v1))<=(x2+(i*v2))):
+            if (x1+(i*v1))==(x2+(i*v2)):
+                count=True
+                break
+            i+=1
+        if count==True:
+            return "YES"
+        else:
+            return "NO"
+    else:
+        return "YES"
