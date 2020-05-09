743b22114df482c3d42a265926f7e79bea8d4fbc
Semert <semert2121@gmail.com>
2020-05-09 21:11:51 +0300

    Between Two Sets Question

---
+++ HackerRank/E - Between Two Sets.py
@@ -0,0 +1,25 @@
+# https: // www.hackerrank.com / challenges / between - two - sets / problem
+
+
+def getTotalX(a, b):
+    # Write your code here
+    x = 0
+    for n in range(1, min(b) + 1):
+        multiple_of_a = True
+
+        for element_a in a:
+            if n % element_a != 0:
+                multiple_of_a = False
+                break
+
+        if multiple_of_a:
+            factor_of_b = True
+            for element_b in b:
+                if element_b % n != 0:
+                    factor_of_b = False
+                    break
+
+            if factor_of_b:
+                x += 1
+
+    return x
