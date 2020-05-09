4dbb56bc8ec51a3054884b1c25cb863003cb93c2
Semert <semert2121@gmail.com>
2020-05-09 21:10:55 +0300

    Fix Question

--- HackerRank/E - Kangaroo.py
+++ HackerRank/E - Kangaroo.py
@@ -12,32 +12,32 @@ def kangaroo(x1, v1, x2, v2):
     return 'NO'
 
 # Solution 2
-i,count=1,False
-    if v1<=v2 and x1<x2:
-        return "NO"
-    elif v1>v2 and x1>x2:
-        return "NO"
-    elif v1>v2 and x1<x2:
-        while((x1+(i*v1))<=(x2+(i*v2))):
-            if (x1+(i*v1))==(x2+(i*v2)):
-                count=True
-                break
-            i+=1
-        if count==True:
-            return "YES"
-        else:
-            return "NO"
-    else:
-        return "YES"
+# i,count=1,False
+#     if v1<=v2 and x1<x2:
+#         return "NO"
+#     elif v1>v2 and x1>x2:
+#         return "NO"
+#     elif v1>v2 and x1<x2:
+#         while((x1+(i*v1))<=(x2+(i*v2))):
+#             if (x1+(i*v1))==(x2+(i*v2)):
+#                 count=True
+#                 break
+#             i+=1
+#         if count==True:
+#             return "YES"
+#         else:
+#             return "NO"
+#     else:
+#         return "YES"
 
 # Solution 3
 #     if (x1 < x2 and v1 < v2) or (x1 > x2 and v1 > v2):
-#         return "NO"
-#     elif ((x1 == x2) and (v1 != v2)) or ((x1 != x2) and (v1 == v2)):
-#         return "NO"
-#     elif (x1 > x2 and v1 > v2) or (x1 < x2 and v1 < v2):
-#         return "NO"
-#     if (x1 - x2) % (v1 - v2) == 0:
-#         return "YES"
-#     else:
-#         return "NO"
+# #         return "NO"
+# #     elif ((x1 == x2) and (v1 != v2)) or ((x1 != x2) and (v1 == v2)):
+# #         return "NO"
+# #     elif (x1 > x2 and v1 > v2) or (x1 < x2 and v1 < v2):
+# #         return "NO"
+# #     if (x1 - x2) % (v1 - v2) == 0:
+# #         return "YES"
+# #     else:
+# #         return "NO"
