9d321fe8397962b943cfe841041226578dac5846
Semert <semert2121@gmail.com>
2020-09-02 21:17:44 +0300

    split

--- Algorithms/Test.py
+++ Algorithms/Test.py
@@ -95,15 +95,15 @@ print([word[i:i + 3] for i in range(0, len(word), 3)])
 # ------------------------------------------------------
 
 
-word = 'geeks, for, geeks, pawan'
-
-# maxsplit: 0
-print(word.split(', ', 0))
-
-# maxsplit: 4
-print(word.split(', ', 4))
-
-# maxsplit: 1
-print(word.split(', ', 1))
+# word = 'geeks, for, geeks, pawan'
+#
+# # maxsplit: 0
+# print(word.split(', ', 0))
+#
+# # maxsplit: 4
+# print(word.split(', ', 4))
+#
+# # maxsplit: 1
+# print(word.split(', ', 1))
 
 # ------------------------------------------------------
