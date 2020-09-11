ca30ad1be66611e30b8b3384262688cc70622930
Semert <semert2121@gmail.com>
2020-09-11 22:38:57 +0300

    study

--- Algorithms/List compherension.py
+++ Algorithms/List compherension.py
@@ -34,11 +34,11 @@ create_arr()
 
 
 # I want 'n' for each 'n' in nums if 'n' is even
-# my_list = []
-# for n in nums:
-#   if n%2 == 0:
-#     my_list.append(n)
-# print(my_list)
+my_list = []
+for n in nums:
+  if n%2 == 0:
+    my_list.append(n)
+print(my_list)
 
 # Using a filter + lambda
 # my_list = filter(lambda n: n%2 == 0, nums)
