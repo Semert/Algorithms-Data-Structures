9fedadb85d22f2663328b54ba75df42731f1562f
Semert <semert2121@gmail.com>
2020-09-30 15:50:01 +0300

    leetcode Tree

---
+++ LeetCode/Vallid Palindrome.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        actual_str = "".join(x for x in s if x.isalnum()).lower()
+
+        def checkPalindrome(s):
+            i = 0
+            j = len(s) - 1
+
+            while (i < j):
+                if (s[i] == s[j]):
+                    j -= 1
+                    i += 1
+                else:
+                    return False
+            return True
+
+        return checkPalindrome(actual_str)
