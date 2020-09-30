d5bf7796de58e63618d3f5e5fba8b8fcdfc1a3a5
Semert <semert2121@gmail.com>
2020-09-30 17:45:57 +0300

    leetcode Missing Number

---
+++ LeetCode/Missing Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        total = 0
+        n = len(nums)
+
+        for i in range(len(nums)):
+            total += nums[i]
+
+        return (n * (n + 1)) / 2 - total
+
