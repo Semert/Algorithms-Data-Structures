1a263096e1d9609f8890d93ff6b2a6f1519dc7bc
Semert <semert2121@gmail.com>
2020-10-01 14:38:39 +0300

    Leet Code Happy Number

---
+++ LeetCode/Happy Number.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/happy-number/submissions/
+class Solution(object):
+    def isHappy(self, n):
+        # arr = [int(x) for x in str(n)]
+        #
+        # toplama = 0
+        # count = 0
+        #
+        # while toplama != 1:
+        #     for i in arr:
+        #         toplama += i ** 2
+        #     count += 1
+        #
+        #     arr = [int(x) for x in str(toplama)]
+        #     if (toplama <= 1):
+        #         return True
+        #     toplama = 0
+        #     if (count > 5):
+        #         return False
+
+        def karenumber(num):
+            result = 0
+            while (num>0):
+                first = num % 10
+                result += first * first
+                num //= 10
+            return result
+
+        Seen = set()
+
+        while karenumber(n) not in Seen:
+            total = karenumber(n)
+            if (total == 1):
+                return True
+            else:
+                Seen.add(total)
+                n = total
+        return False
+
--- LeetCode/question.py
+++ LeetCode/question.py
@@ -15,3 +15,11 @@ countNum(num,c)
 countNum(30,"1")
 countNum(50,"7")
 # num ve c değerlerini değiştirip test edebilirsiniz. [num -> int, c -> str]
+
+arr = [1, 2, 3]
+
+if 1 not in arr:
+    print("hahaha")
+if 1 in arr:
+    print("ok")
+
