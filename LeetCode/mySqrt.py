72c5dffd724ee37f0359ed01e6befa0b1463a0eb
Semert <semert2121@gmail.com>
2020-10-05 14:42:32 +0300

    Leetcode

---
+++ LeetCode/mySqrt.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/sqrtx/submissions/
+class Solution(object):
+    def mySqrt(self, x):
+
+        left, right = 0, x
+
+        while left <= right:
+
+            mid = (right + left) // 2
+            square = mid ** 2
+
+            if square <= x:
+                left = mid + 1
+
+            elif square > x:
+                right = mid - 1
+
+        return left - 1
