b7c67ab0b785d4acd7687e9cd6c3f9b770804067
Semert <semert2121@gmail.com>
2020-10-06 16:00:18 +0300

    LeetCode

---
+++ LeetCode/Num Unique Emails.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/unique-email-addresses/submissions/
+class Solution(object):
+    def numUniqueEmails(self, emails):
+
+        newSet = set()
+        for email in emails:
+
+            local = email[:email.index("@")]
+            domain = email[email.index("@"):]
+
+            if "+" in local:
+                local = local[:local.index("+")]
+
+            local = local.replace(".", "")
+
+            newSet.add(local + domain)
+        return (len(newSet))
+
+
