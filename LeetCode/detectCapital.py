bf511ea4fb9e7487edf184de5838d00802281c1f
Semert <semert2121@gmail.com>
2020-10-07 17:47:28 +0300

    LeetCode

---
+++ LeetCode/detectCapital.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/detect-capital/submissions/
+class Solution(object):
+    def detectCapitalUse(self, word):
+
+        count = 0
+
+        for char in word:
+
+            if char.isupper():
+                count += 1
+
+        return count == len(word) or count == 0 or (count == 1 and word[0].isupper())
