84c9a71d9bb7ca1c172e1d208755124e0204ed57
Semert <semert2121@gmail.com>
2020-12-06 19:20:20 +0300

    CodeSignal

---
+++ CodeSignal/commonCharacterCount.py
@@ -0,0 +1,14 @@
+# https://app.codesignal.com/arcade/intro/level-3/JKKuHJknZNj4YGL32
+def commonCharacterCount(s1, s2):
+    arr = []
+
+    for i in set(s1):
+        arr.append(min(s1.count(i), s2.count(i)))
+
+    return (sum(arr))
+
+
+
+
+
+
