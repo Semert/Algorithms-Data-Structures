2d8fb3af6aad035f632e2ad1b80a79ce58cc2fec
Semert <semert2121@gmail.com>
2020-12-13 11:20:41 +0300

    CodeSignal

---
+++ CodeSignal/isIPv4Address.py
@@ -0,0 +1,25 @@
+def isIPv4Address(s):
+    
+    # p = s.split('.')
+    # return len(p) == 4 and all(n.isdigit() and 0 <= int(n) < 256 for n in p)
+
+    sl = s.split('.')
+    if len(sl) != 4:
+        return False
+
+    for s in sl:
+        if not s.isdigit():
+            return False;
+        a = int(s)
+        if a > 255 or a < 0:
+            return False
+
+        if len(s) == 2 and s[0] == "0":
+            return False
+
+    return True;
+
+
+
+
+
