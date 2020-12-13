9aa71b4359beb1d704f5ea33c329acffdd39795f
Semert <semert2121@gmail.com>
2020-12-13 11:58:10 +0300

    CodeSignal

--- Algorithms/Array Replace.py
+++ Algorithms/Array Replace.py
@@ -1,9 +1,10 @@
+# https://app.codesignal.com/arcade/intro/level-6/mCkmbxdMsMTjBc3Bm/solutions
 def arrayReplace(inputArray, elemToReplace, substitutionElem):
 
     # return [substitutionElem if i == elemToReplace else i for i in inputArray]
 
     arr = []
-    
+
     for i in inputArray:
         if i == elemToReplace:
             arr.append(substitutionElem)
